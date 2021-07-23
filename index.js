c7c34709c629bf55ff14b31fd5b17c1ed33377f9
hung vu <hungvu@fortna.com>
2021-07-23 18:39:06 +0700

    add log

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { spawnSync, execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const ora = require('ora');
+const chalk = require('chalk')
 
 const repoPath = path.resolve(__dirname, 'repository');
 const extensionPath = path.resolve(__dirname, 'extension');
@@ -17,11 +18,22 @@ const args = [
 
 const spinner = ora('Loading resource...').start();
 
+console.log('Hello world!')
+console.log(repoPath)
+console.log(process.cwd())
+console.log(__dirname)
+
 execSync(`rm -rf ${repoPath}`);
 const child = spawnSync("git", args);
 
+console.log('Hello world!222')
+
 console.log(`${child.stderr}`);
-execSync(`rm -rf ${extensionPath} && cd ${repoPath} && mkdir ../extension && cp -r extension/* ../extension && rm -rf ${repoPath}`);
+execSync(`rm -rf ${extensionPath} && mkdir ${__dirname}/extension && cd ${repoPath} && cp -r extension/* ../extension && rm -rf ${repoPath}`);
+
+
+console.log('Hello world!333')
+
 
 execa('node', ['chrome-launch-exc.js']).stdout.pipe(process.stdout);
 
